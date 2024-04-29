d5c7a28e8165a3e015b616de5ffd75663875d781
sripwoud <me@sripwoud.xyz>
2024-04-29 21:38:36 +0200

    refactor: format giga

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.saturating_add(Duration::seconds(1000000000))
+    start.saturating_add(Duration::seconds(1_000_000_000))
 }
