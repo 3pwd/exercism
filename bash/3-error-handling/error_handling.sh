0510f1802c6649b32b6eaa7ceff16eea6d5d2c9e
sripwoud <me@sripwoud.xyz>
2024-04-30 23:03:20 +0200

    chore: number `bash` exercises

---
+++ bash/3-error-handling/error_handling.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+main () {
+  if [[ $# = 1 ]];then
+    echo "Hello, $1"
+    return 0
+  else
+    echo "Usage: error_handling.sh <person>"
+    return 1
+  fi
+}
+
+
+main "$@"
+
