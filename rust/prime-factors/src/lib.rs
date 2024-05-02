fbcd80b2f000449025d8e7964c96084b5c6e46eb
sripwoud <me@sripwoud.xyz>
2024-05-02 22:21:35 +0200

    refactor: use `Iterator` trait implementation

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,15 +1,32 @@
-pub fn factors(n: u64) -> Vec<u64> {
-    let mut n = n;
-    let mut result: Vec<u64> = Vec::new();
-    let mut current_factor = 2;
+struct PrimeFactors {
+    current_factor: u64,
+    n: u64,
+}
 
-    while n > 1 {
-        while n % current_factor == 0 {
-            result.push(current_factor);
-            n = n / current_factor;
+impl PrimeFactors {
+    pub fn new(n: u64) -> Self {
+        Self {
+            current_factor: 2,
+            n,
         }
-        current_factor += 1;
     }
+}
 
-    result
+impl Iterator for PrimeFactors {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.n > 1 {
+            if self.n % self.current_factor == 0 {
+                self.n = self.n / self.current_factor;
+                return Some(self.current_factor);
+            }
+            self.current_factor += 1;
+        }
+        None
+    }
+}
+
+pub fn factors(n: u64) -> Vec<u64> {
+    PrimeFactors::new(n).collect()
 }
