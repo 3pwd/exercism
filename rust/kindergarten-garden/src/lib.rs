1b84217cad248e9ab9d9334cb7a2f588d5e110a3
sripwoud <me@sripwoud.xyz>
2024-05-03 21:08:54 +0200

    refactor `kindergarten-garden`

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,56 +1,28 @@
-use std::collections::HashMap;
-
 const STUDENTS: [&str; 12] = [
     "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
     "Kincaid", "Larry",
 ];
-const PLANTS: [&str; 4] = ["violets", "radishes", "grass", "clover"];
 
-pub struct Garden<'a> {
-    layout: [[char; 4]; 12],
-    plants: HashMap<char, &'a str>,
-    students: HashMap<&'a str, usize>,
+pub fn get_plant(c: char) -> &'static str {
+    match c {
+        'V' => "violets",
+        'R' => "radishes",
+        'G' => "grass",
+        _ => "clover",
+    }
 }
 
-impl Garden<'static> {
-    pub fn new(diagram: String) -> Self {
-        let mut plants = HashMap::new();
-        let mut students = HashMap::new();
+pub fn get_plants(s: &str) -> Vec<&'static str> {
+    s.chars().map(get_plant).collect()
+}
 
-        let mut layout: [[char; 4]; 12] = [[' '; 4]; 12];
-        for (i, line) in diagram.lines().enumerate() {
-            for (j, chunk) in line.chars().collect::<Vec<char>>().chunks(2).enumerate() {
-                layout[j][i * 2] = chunk[0];
-                layout[j][i * 2 + 1] = chunk[1];
-            }
-        }
-        for plant in &PLANTS {
-            plants.insert(plant.chars().next().unwrap(), *plant);
-        }
-
-        for (i, student) in STUDENTS.iter().enumerate() {
-            students.insert(*student, i);
-        }
-
-        Self {
-            layout,
-            plants,
-            students,
-        }
-    }
-
-    fn plants(&self, student: &str) -> Vec<&'static str> {
-        let index = self
-            .students
-            .get(student)
-            .expect("this student is not in the class");
-        self.layout[*index]
-            .iter()
-            .map(|&plant| self.plants[&plant])
-            .collect()
-    }
+pub fn find_cup_idx(student: &str) -> usize {
+    STUDENTS.iter().position(|&x| x == student).unwrap() * 2
 }
 
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    Garden::new(diagram.to_lowercase()).plants(student)
+    diagram
+        .lines()
+        .flat_map(|l| l.chars().skip(find_cup_idx(student)).take(2).map(get_plant))
+        .collect()
 }
