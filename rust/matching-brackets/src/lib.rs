aaf329506c317bcdb6e56366ae63c6a2fc8d8908
sripwoud <me@sripwoud.xyz>
2024-05-06 16:27:09 +0200

    feat: solve `matching-brackets` (#10)

---
+++ rust/matching-brackets/src/lib.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+const BRACKET_PAIRS: [(char, char); 4] = [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')];
+
+struct Brackets {
+    brackets: Vec<char>,
+    pairs: HashMap<char, char>,
+    stack: Vec<char>,
+}
+
+impl Brackets {
+    fn new(string: &str) -> Self {
+        let brackets = string
+            .chars()
+            .filter(|&ch| BRACKET_PAIRS.iter().any(|&(l, r)| l == ch || r == ch))
+            .collect::<Vec<char>>();
+        Self {
+            brackets,
+            pairs: HashMap::from(BRACKET_PAIRS),
+            stack: Vec::new(),
+        }
+    }
+
+    fn are_balanced(&mut self) -> bool {
+        for &ch in &self.brackets {
+            if let Some(&expected_closing_bracket) = self.pairs.get(&ch) {
+                self.stack.push(expected_closing_bracket);
+            } else {
+                if self.stack.pop() != Some(ch) {
+                    return false;
+                }
+            }
+        }
+        self.stack.is_empty()
+    }
+}
+
+pub fn brackets_are_balanced(string: &str) -> bool {
+    Brackets::new(string).are_balanced()
+}
