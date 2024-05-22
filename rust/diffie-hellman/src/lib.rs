150136cec315e657db1153c4acacefab8e838d80
sripwoud <me@sripwoud.xyz>
2024-05-22 09:07:51 +0200

    refactor

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,11 +1,11 @@
 use rand::{thread_rng, Rng};
 
 trait Power {
-    fn pow_by_squaring_modulo(self, exp: u64, modulo: u64) -> u64;
+    fn powmod(self, exp: u64, modulo: u64) -> u64;
 }
 
 impl Power for u64 {
-    fn pow_by_squaring_modulo(self, exp: u64, modulo: u64) -> u64 {
+    fn powmod(self, exp: u64, modulo: u64) -> u64 {
         let (mut base, mut exp, modulo) = (self as u128, exp as u128, modulo as u128);
         let mut result = 1;
         while exp > 0 {
@@ -24,9 +24,9 @@ pub fn private_key(p: u64) -> u64 {
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    g.pow_by_squaring_modulo(a, p)
+    g.powmod(a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    public_key(p, b_pub, a)
+    b_pub.powmod(a, p)
 }
