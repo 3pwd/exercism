1c2d09e7724bac9ecbca0d84502d27d0f1de6a24
sripwoud <me@sripwoud.xyz>
2024-08-29 17:00:51 +0200

    refactor encode

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,47 +1,39 @@
 pub fn encode(source: &str) -> String {
-    let mut result = source
+    // appending a char to avoid having to deal with the last char
+    format!("{source}#")
         .chars()
-        .scan((None, 0), |(last_char, count): &mut(std::option::Option<std::primitive::char>, i32), char| {
-            let result = match *last_char {
-                //matching character
-                Some(c) if c == char => {
-                    *count += 1;
-                    // we don't emit anything yet
-                    String::new()
-                }
-                Some(c) => {
-                    // new character
-                    let previous_run = if *count > 1 {
-                        format!("{count}{c}")
-                    } else {
-                        c.to_string()
-                    };
+        .scan(
+            (None, 0),
+            |(last_char, count): &mut (std::option::Option<std::primitive::char>, i32), char| {
+                let result = match *last_char {
+                    Some(c) if c != char => {
+                        // new character
+                        let previous_run = format!(
+                            "{}{}",
+                            if *count > 1 {
+                                count.to_string()
+                            } else {
+                                String::new()
+                            },
+                            c
+                        );
 
-                    *count = 1;
-                    previous_run
-                }
-                None => {
-                    *count = 1;
-                    String::new()
-                }
-            };
+                        *count = 1;
+                        previous_run
+                    }
+                    //matching character or first one
+                    _ => {
+                        *count += 1;
+                        // we don't emit anything yet
+                        String::new()
+                    }
+                };
 
-            *last_char = Some(char);
-            Some(result)
-        })
-        .collect::<String>();
-
-    // emit last run
-    if let Some(last_char) = source.chars().last() {
-        let last_count = source.chars().rev().take_while(|&c| c == last_char).count();
-        if last_count > 1 {
-            result.push_str(&format!("{last_count}{last_char}"));
-        } else {
-            result.push_str(&format!("{last_char}"));
-        }
-    }
-
-    result
+                *last_char = Some(char);
+                Some(result)
+            },
+        )
+        .collect::<String>()
 }
 
 pub fn decode(source: &str) -> String {
