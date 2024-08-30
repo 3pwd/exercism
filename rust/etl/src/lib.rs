34347958b39d137bd5c0446d903f73a7be117a7a
sripwoud <me@sripwoud.xyz>
2024-08-30 18:13:59 +0200

    refactor

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,14 +1,10 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut th: BTreeMap<char, i32> = BTreeMap::new();
-
-    for (score, letters) in h.iter() {
-        for letter in letters.iter() {
-            let lowercase_letter = letter.to_ascii_lowercase();
-            th.insert(lowercase_letter, *score);
-        }
-    }
-
-    th
+    h.iter().fold(BTreeMap::new(), |mut t, (score, letters)| {
+        letters.iter().for_each(|c| {
+            t.insert(c.to_ascii_lowercase(), *score);
+        });
+        t
+    })
 }
