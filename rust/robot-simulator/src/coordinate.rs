607e09faaf6e3772b10646f316c8dd1d1d5a3819
sripwoud <me@sripwoud.xyz>
2024-09-12 16:34:26 +0200

    refactor: use macro rules to impl coordinates

--- rust/robot-simulator/src/coordinate.rs
+++ rust/robot-simulator/src/coordinate.rs
@@ -1,21 +1,19 @@
 use std::ops::Add;
 
-#[derive(Clone, Copy)]
-pub struct X(pub i32);
-impl Add<i32> for X {
-    type Output = Self;
+macro_rules! coordinate {
+    ($name:ident) => {
+        #[derive(Clone, Copy)]
+        pub struct $name(pub i32);
 
-    fn add(self, other: i32) -> Self {
-        Self(self.0 + other)
-    }
+        impl Add<i32> for $name {
+            type Output = Self;
+
+            fn add(self, other: i32) -> Self {
+                Self(self.0 + other)
+            }
+        }
+    };
 }
 
-#[derive(Clone, Copy)]
-pub struct Y(pub i32);
-impl Add<i32> for Y {
-    type Output = Self;
-
-    fn add(self, other: i32) -> Self {
-        Self(self.0 + other)
-    }
-}
+coordinate!(X);
+coordinate!(Y);
