4c9164fa77774e034d87e5713e8b549d4ce3f667
sripwoud <me@sripwoud.xyz>
2024-09-17 14:41:19 +0200

    refactor

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,19 +1,10 @@
 use std::cmp::Ordering;
 
-fn binary_search(array: &[i32], key: i32, start: usize, end: usize) -> Option<usize> {
-    if start >= end {
-        return None;
-    };
-
-    let mid = start + (end - start) / 2;
-
-    match array[mid].cmp(&key) {
-        Ordering::Equal => Some(mid),
-        Ordering::Less => binary_search(array, key, mid + 1, end),
-        Ordering::Greater => binary_search(array, key, start, mid),
-    }
-}
-
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    binary_search(array, key, 0, array.len())
+    let mid = array.len() / 2;
+    match key.cmp(array.get(mid)?) {
+        Ordering::Equal => Some(mid),
+        Ordering::Less => find(&array[..mid], key),
+        Ordering::Greater => find(&array[mid + 1..], key).map(|i| i + mid + 1),
+    }
 }
